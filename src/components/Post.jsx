3cd501035bad7f76c02be08c010b2f593a9e3791
Katrina Hockman and Quinn Miller <quinn@>
2019-02-19 13:16:50 -0800

    add button to show username of post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,76 +1,77 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
+const feedGrid = {
+  display: 'grid',
+  gridTemplateColumns: '1fr 3fr',
+  textAlign: 'left',
+}
 
-function Post(props) {
-  let detailValue = false;
-  const feedGrid = {
-    display: 'grid',
-    gridTemplateColumns: '1fr 3fr',
-    textAlign: 'left',
+const imageStyle = {
+  margin: '15',
+  width: '75',
+  borderRadius: '50%',
+  borderStyle: 'solid',
+  borderColor: 'rgba(255, 255, 255, .2)'
+}
+
+
+
+const usernameStyle = {
+  fontSize: '22',
+  marginBottom: '0',
+  fontWeight: 'bold',
+}
+
+const handleStyle = {
+  fontWeight: 'lighter',
+  fontSize: '16',
+  marginBottom: '5',
+  color: 'gray',
+  marginBottom: '0'
+}
+
+const paragraphStyle = {
+  fontWeight: 'lighter',
+  marginTop: '6'
+};
+
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      details: false
+    };
   }
 
-  const imageStyle = {
-    margin: '15',
-    width: '75',
-    borderRadius: '50%',
-    borderStyle: 'solid',
-    borderColor: 'rgba(255, 255, 255, .2)'
-  }
 
-
-
-  const usernameStyle = {
-    fontSize: '22',
-    marginBottom: '0',
-    fontWeight: 'bold',
-  }
-
-  const handleStyle = {
-    fontWeight: 'lighter',
-    fontSize: '16',
-    marginBottom: '5',
-    color: 'gray',
-    marginBottom: '0'
-  }
-
-  const paragraphStyle = {
-    fontWeight: 'lighter',
-    marginTop: '6'
-  };
-
-  function toggleDetails() {
-    console.log(detailValue)
-    if (detailValue === false) {
-      detailValue = true;
+  toggleDetails() {
+    console.log(this.state.details)
+    if (this.state.details === false) {
+      this.setState({details: true})
     } else {
-      detailValue = false;
+      this.setState({details: false})
     }
   }
 
-  function getDetails(id) {
-    props.onGetDetails(id);
-  }
-
-  return (
-
-    <div>
-      <div style={feedGrid}>
-        <img src={props.image} style={imageStyle}></img>
-        <div>
-          <h2 style={usernameStyle}>{props.username} <span style={handleStyle}>{props.handle}</span></h2>
-          <p style={paragraphStyle}>{props.content}</p>
-        </div>
-        <p>Likes: {props.likes}</p>
-        <button onClick={()=> toggleDetails()}>show details</button>
-          {detailValue ?
-            <p>"hello"</p>:
-              null
+  render() {
+    return (
+      <div>
+        <div style={feedGrid}>
+          <img src={this.props.image} style={imageStyle}></img>
+          <div>
+            <h2 style={usernameStyle}>{this.props.username} <span style={handleStyle}>{this.props.handle}</span></h2>
+            <p style={paragraphStyle}>{this.props.content}</p>
+          </div>
+          <p>Likes: {this.props.likes}</p>
+          <button onClick={()=> this.toggleDetails()}>show details</button>
+          {this.state.details ? <p>{this.props.username}</p>:null
           }
-      </div>
+        </div>
 
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
 
